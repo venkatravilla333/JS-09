0012cf476e7bd8955286cbc0bb556bf62a591ca0
konda ravilla <kondaravilla000@gmail.com>
2024-08-21 12:56:14 +0530

    first commit

---
+++ Basics/index.js
@@ -0,0 +1,34 @@
+
+
+// var a = 'sachin'
+
+// function test() {
+//   var x = 100
+// }
+
+// test()
+
+// document.write(a)
+
+// console.log(a)
+
+//data types
+
+// primitives
+
+var a = 'sachin' //string
+console.log(a, typeof a)
+
+var b = 40 //number
+console.log(b, typeof b)
+
+var c = true//boolean
+console.log(c, typeof c)
+
+var d // undefined
+console.log(d, typeof d)
+
+var e = null //null
+console.log(e, typeof e)
+
+
