e8b49e2bfda80482cd3111f62773f74c9a649064
konda ravilla <kondaravilla000@gmail.com>
2024-08-23 13:19:45 +0530

    basics completed

--- Basics/index.js
+++ Basics/index.js
@@ -16,19 +16,70 @@
 
 // primitives
 
-var a = 'sachin' //string
-console.log(a, typeof a)
+// var a = 'sachin' //string
+// console.log(a, typeof a)
 
-var b = 40 //number
-console.log(b, typeof b)
+// var b = 40 //number
+// console.log(b, typeof b)
 
-var c = true//boolean
-console.log(c, typeof c)
+// var c = true//boolean
+// console.log(c, typeof c)
 
-var d // undefined
-console.log(d, typeof d)
+// var d // undefined
+// console.log(d, typeof d)
 
-var e = null //null
-console.log(e, typeof e)
+// var e = null //null
+// console.log(e, typeof e)
+
+//reference
+
+//object
+
+var person = {
+  name: 'sachin',
+  age: 40,
+  play: function () {
+  
+  }
+}
+
+console.log(person, typeof person)
+
+//how to access object values
+// console.log(person.name)
+// console.log(person.age)
+// console.log(person.play)
+// console.log(person.play())
+// console.log(person['name'])
+// console.log(person['age'])
+// console.log(person['play']())
+
+// Array
+
+var names = ['sachin', 'kohli', 'dhoni', true, undefined, {age: 40}, function test(){console.log('test')}]
+
+console.log(names, typeof names)
+console.log(Array.isArray(names))
+// console.log(names[0])
+// console.log(names[1])
+// console.log(names[2])
+// console.log(names[3])
+// console.log(names[4])
+// console.log(names[5])
+// names[6]()
+
+//function
+
+var x = 10
+
+function test() {
+  var x = 100
+}
+test()
+console.log(typeof test)
+
+
+
+
 
 
