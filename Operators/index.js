d43664d3fb69d0f31c735727562ad0cf1a80c4c2
konda ravilla <kondaravilla000@gmail.com>
2024-08-24 19:09:18 +0530

    operators

---
+++ Operators/index.js
@@ -0,0 +1,100 @@
+
+
+//Arithmatic operators
+
+// var x = 20
+// var y = 10
+
+// console.log(x + y) //add
+// console.log(x - y) //subtraction
+// console.log(x * y) //multifly
+// console.log(x / y) //division
+// console.log(x % y) //remainder
+// console.log(x ** y) //exponential
+
+// console.log(x)
+// console.log(++x) //pre increment
+// console.log(x++) //post increment
+// console.log(x)
+// console.log(--x) //pre decrement
+// console.log(x--) //post decrement
+// console.log(x)
+
+//Assignment operator
+
+// var x = 20
+// console.log(x)
+// x++
+// x++
+// x++
+// x++
+// x++
+
+// x += 5
+// console.log(x)
+// x -= 5
+// console.log(x)
+// x *= 5
+// console.log(x)
+// x /= 5
+// console.log(x)
+// x %= 5
+// console.log(x)
+// x **= 5
+// console.log(x)
+
+//comparision operrators
+
+// var x = 10
+// var y = 9
+
+//relational
+
+// console.log(x > y)
+// console.log(x >= y)
+// console.log(x < y)
+// console.log(x <= y)
+
+//equality operators
+
+//primitives
+
+// var x = 10
+// var y = 10
+// var y = 9
+// var y = '10'
+
+//reference
+// var x = {value: 10}
+// var x = {value: 10}
+// var y = {value: 10}
+// var y = {value: 20}
+// var y = x
+
+// var x = []
+// var y = []
+
+// var x = ()=>{}
+// var y = ()=>{}
+
+// var x = 10
+// var y = {value: 10}
+
+// var x = null
+// var y = undefined
+
+// console.log(x == y) //loose equality
+// console.log(x === y) //strict equality
+
+// inequality 
+// console.log(x != y) //loose inequality
+// console.log(x !== y) //strict inequality
+
+
+
+
+
+
+
+
+
